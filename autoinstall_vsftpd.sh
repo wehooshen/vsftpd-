9fb7c6c23f924c285cffc6a5308181c24f0e2dd3
HYYH169 <35693536+HYYH169@users.noreply.github.com>
2018-05-16 13:22:14 +0800

    Update and rename test.sh to autoinstall_vsftpd.sh

--- autoinstall_vsftpd.sh
+++ autoinstall_vsftpd.sh
@@ -1,4 +1,4 @@
-#version1.0 此版本只适合centos7自动配置可用的vsftpd
+#version1.1 此版本只适合centos7自动配置可用的vsftpd
 yum install -y  vsftpd 
 touch vsftpd.conf
 echo  -e " anonymous_enable=NO\n local_enable=YES\n write_enable=YES\n local_umask=022\n dirmessage_enable=YES\n xferlog_enable=YES\n connect_from_port_20=NO\n port_enable=NO\n pasv_enable=YES\n pasv_min_port=10050\n pasv_max_port=10060\n pasv_promiscuous=YES\n xferlog_std_format=YES\n chroot_local_user=YES\n chroot_list_enable=YES\n chroot_list_file=/etc/vsftpd/chroot_list\n listen=YES\n listen_ipv6=NO\n pam_service_name=vsftpd\n userlist_enable=NO\n tcp_wrappers=YES\n " > vsftpd.conf
